0bee4776628bc0010bcd869420a7674c7781d7d1
Rajae <hemmanyrajae@gmail.com>
2024-10-10 03:32:28 +0000

    update

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,28 +1,20 @@
-class Teacher {
-    private _firstName: string;
-    private _lastName: string;
-    public fullTimeEmployee: boolean;
-    public yearsOfExperience?: number; // Optional
-    public location: string;
-
-    [key: string]: any; // Index signature
-
-    constructor(firstName: string, lastName: string, fullTimeEmployee: boolean, location: string, yearsOfExperience?: number) {
-        this._firstName = firstName;
-        this._lastName = lastName;
-        this.fullTimeEmployee = fullTimeEmployee;
-        this.location = location;
-        if (yearsOfExperience) {
-            this.yearsOfExperience = yearsOfExperience;
-        }
-    }
-
-    get firstName(): string {
-        return this._firstName;
-    }
-    get lastName(): string {
-        return this._lastName;
-    }
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number; // optional
+  location: string;	
+  [key: string]: any;
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
 
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
